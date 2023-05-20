f1f0e28628aa4424b897bae7089f96d0068d2606
gluay88 <p.wantagaew@gmail.comm>
2023-05-20 11:05:43 -0600

    add ADD and REMOVE payloaduseReducer

--- src/components/Products.js
+++ src/components/Products.js
@@ -34,6 +34,49 @@ const Products = ({ state, dispatch }) => {
               <span>{prod.title}</span>
               <b>$ {prod.price}</b>
             </div>
+            {cart.some((p) => p.id === prod.id) ? (
+              <button
+                style={{
+                  padding: 5,
+                  border: 0,
+                  borderRadius: 5,
+                  backgroundColor: "#e53935",
+                  color: "white",
+                }}
+                onClick={() =>
+                  dispatch({
+                    type: "REMOVE_FROM_CART",
+                    payload: prod,
+                  })
+                }
+              >
+                Remove
+              </button>
+            ) : (
+              <button
+                style={{
+                  padding: 5,
+                  border: 0,
+                  borderRadius: 5,
+                  backgroundColor: "green",
+                  color: "white",
+                }}
+                onClick={() =>
+                  dispatch({
+                    type: "ADD_TO_CART",
+                    payload: {
+                      id: prod.id,
+                      title: prod.title,
+                      thumbnail: prod.thumbnail,
+                      qty: 1,
+                      price: prod.price,
+                    },
+                  })
+                }
+              >
+                Add to Cart
+              </button>
+            )}
           </div>
         );
       })}
